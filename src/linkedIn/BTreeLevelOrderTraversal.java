09aeb4bce617a03c35e1546ba4a76407f32fa736
rutvijdhotey <rutvij.dhotey@gmail.com/Users/rutvijdhotey/Documents/Sofwares/apache-ant-1.9.6/bin/antRun>
2017-01-06 16:43:42 -0800

    BT Level Order Traversal

--- src/linkedIn/BTreeLevelOrderTraversal.java
+++ src/linkedIn/BTreeLevelOrderTraversal.java
@@ -1,5 +1,56 @@
 package linkedIn;
 
-public class BTreeLevelOrderTraversal {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+public class BTreeLevelOrderTraversal {
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	
+	public class Solution {
+	    public List<List<Integer>> levelOrder(TreeNode root) {
+	        List<List<Integer>> mainList = new ArrayList<>();
+	        if(root == null){
+	            return mainList;
+	        }
+	        
+	        LinkedList<LinkedList<TreeNode>> mainTrees = new LinkedList<>();
+	        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+	        List<Integer> values = new ArrayList<Integer>();
+	        
+	        if(root!= null){
+	            current.add(root);
+	            values.add(root.val);
+	        }
+	        
+	        while(current.size() > 0){
+	            mainTrees.add(current);
+	            mainList.add(values);
+	            
+	            LinkedList<TreeNode> parents = current;
+	            values = new ArrayList<>();
+	            current = new LinkedList<>();
+	            
+	            for(TreeNode parent : parents){
+	                if(parent.left != null){
+	                    values.add(parent.left.val);
+	                    current.add(parent.left);
+	                }
+	                if(parent.right != null){
+	                    values.add(parent.right.val);
+	                    current.add(parent.right);
+	                }
+	            }
+	        }
+	        
+	        return mainList;
+	        
+	        
+	    }
+	}
 }
