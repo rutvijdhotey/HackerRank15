f723c4ba2e970258ead6fadf92e94c4706139713
rutvijdhotey <rutvij.dhotey@gmail.com>
2017-01-14 22:09:53 -0800

    new problems

---
+++ src/google/EvaluateDivision.java
@@ -0,0 +1,20 @@
+package google;
+
+import java.util.HashSet;
+
+public class EvaluateDivision {
+
+	 public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+		 double[] ans = new double[queries.length];
+		 HashSet<String> variables = new HashSet<String>();
+		 HashMap<String, Double> eqs = new HashMap<String,Double>();
+		 for(String[] eq : equations){
+			 String a = eq[0];
+			 String b = eq[1];
+			 
+		 }
+		 
+		 return ans;
+		 
+	 }
+}
