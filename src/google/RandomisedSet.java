37b6ab8015cfd689cf845c1460e70d38e80c5bb1
rutvijdhotey <rutvij.dhotey@gmail.com>
2017-01-16 15:00:00 -0800

    insert delete add O(1) time

---
+++ src/google/RandomisedSet.java
@@ -0,0 +1,22 @@
+package google;
+
+public class RandomisedSet {
+public RandomisedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        
+    }
+}
