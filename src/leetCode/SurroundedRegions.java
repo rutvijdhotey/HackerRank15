fa881416476028e1ebf477f1d75a926ee616cb74
rutvijdhotey <rutvij.dhotey@gmail.com>
2017-01-25 22:45:16 -0800

    surrounded Islands DFS Question

---
+++ src/leetCode/SurroundedRegions.java
@@ -0,0 +1,43 @@
+package leetCode;
+
+public class SurroundedRegions {
+	public void solve(char[][] board) {
+		if (board.length < 2) {
+			return;
+		}
+
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (i == 0 || i == board.length-1 || j == 0 || j == board.length-1) {
+					dfs(board, i, j);
+				}
+			}
+		}
+		
+		
+		for(int i = 0; i < board.length ; i++){
+			for(int j = 0; j < board[0].length; j++){
+				if(board[i][j]== '*'){
+					board[i][j] = 'O';
+				} else if(board[i][j] == 'O'){
+					board[i][j] = 'X';
+				}
+			}
+		}
+
+	}
+
+	private void dfs(char[][] board, int i, int j) {
+
+		board[i][j] = '*';
+		int[][] dxdy = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
+		for (int[] xy : dxdy) {
+			int dx = i + xy[0];
+			int dy = j + xy[1];
+			if (dx > 0 && dx < board.length && dy > 0 && dy < board[0].length && board[dx][dy] == 'O') {
+				dfs(board, dx, dy);
+			}
+		}
+
+	}
+}
