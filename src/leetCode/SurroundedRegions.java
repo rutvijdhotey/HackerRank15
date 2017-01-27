a163fc03ea4f4ffc94c519057e6d3ed2016c716e
rutvijdhotey <rutvij.dhotey@gmail.com>
2017-01-26 21:27:48 -0800

    DecodeWaysForStringNumToAlphabet

--- src/leetCode/SurroundedRegions.java
+++ src/leetCode/SurroundedRegions.java
@@ -8,19 +8,18 @@ public class SurroundedRegions {
 
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
-				if (i == 0 || i == board.length-1 || j == 0 || j == board.length-1) {
+				if ((i == 0 || i == board.length - 1 || j == 0 || j == board[0].length - 1) && board[i][j] == 'O') {
 					dfs(board, i, j);
 				}
 			}
 		}
-		
-		
-		for(int i = 0; i < board.length ; i++){
-			for(int j = 0; j < board[0].length; j++){
-				if(board[i][j]== '*'){
-					board[i][j] = 'O';
-				} else if(board[i][j] == 'O'){
+
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j] == 'O') {
 					board[i][j] = 'X';
+				} else if (board[i][j] == '*') {
+					board[i][j] = 'O';
 				}
 			}
 		}
